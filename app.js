b3cd7008f9bf25709f6b26f716cd9cca7708b574
emmaneely <emmaneely3@gmail.com>
2022-06-13 11:48:52 -0500

    Completed exercise 1

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+function arraySum(array) {
+    let sum = 0;
+    array.forEach((number) => {
+        sum += number;
+    });
+    return sum;
+}
+console.log(arraySum(numbers));
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
 
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
